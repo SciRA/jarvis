404dfa95ee6ce1a7d487d96c935722c0e7c6f477
Alexandru Coman <acoman@cloudbasesolutions.com>
2015-06-14 15:39:49 +0300

    Add support for global configuration

--- watson/common/constant.py
+++ watson/common/constant.py
@@ -1,1 +1,24 @@
 """Shared constants across the Watson project."""
+import logging
+
+
+ENVIRONMENT = {
+    "development": {
+        "log.name": "bcbiovm-devel",
+        "log.cli_level": logging.DEBUG,
+        "log.file_level": logging.DEBUG,
+        "log.format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        "log.file": None,
+        "misc.attempts": 3,
+        "misc.retry_interval": 0.1,
+    },
+    "production": {
+        "log.name": "bcbiovm",
+        "log.cli_level": logging.INFO,
+        "log.file_level": logging.INFO,
+        "log.format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        "log.file": None,
+        "misc.attempts": 5,
+        "misc.retry_interval": 0.2,
+    }
+}
